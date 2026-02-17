39236bb9fe97d17f8fce6d531b1c294b05b5b95a
Jesus Alvarez Sombrerero <dontloseyourheadsu@gmail.com>
2026-02-17 13:56:51 -0600

    rigid body 2d movement and input

---
+++ RoguelikeDarling/RoguelikeDarling.Core/RigidBody2D/RigidBody2DPlayerInputSystem.cs
@@ -0,0 +1,60 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using RoguelikeDarling.Core.Ecs;
+
+namespace RoguelikeDarling.Core.RigidBody2D
+{
+    public sealed class RigidBody2DPlayerInputSystem : IGameSystem
+    {
+        public void Update(World world, GameTime gameTime)
+        {
+            KeyboardState keyboardState = Keyboard.GetState();
+
+            for (int i = 0; i < world.Entities.Count; i++)
+            {
+                Entity entity = world.Entities[i];
+                if (!entity.TryGetComponent<PlayerControllerComponent>(out PlayerControllerComponent controller)
+                    || !entity.TryGetComponent<RigidBody2DComponent>(out RigidBody2DComponent rigidBody)
+                    || controller == null
+                    || rigidBody == null
+                    || rigidBody.IsStatic)
+                {
+                    continue;
+                }
+
+                Vector2 direction = Vector2.Zero;
+
+                if (keyboardState.IsKeyDown(Keys.A))
+                {
+                    direction.X -= 1f;
+                }
+
+                if (keyboardState.IsKeyDown(Keys.D))
+                {
+                    direction.X += 1f;
+                }
+
+                if (keyboardState.IsKeyDown(Keys.W))
+                {
+                    direction.Y -= 1f;
+                }
+
+                if (keyboardState.IsKeyDown(Keys.S))
+                {
+                    direction.Y += 1f;
+                }
+
+                if (direction != Vector2.Zero)
+                {
+                    direction.Normalize();
+                }
+
+                rigidBody.Velocity = direction * controller.MoveSpeedPixelsPerSecond;
+            }
+        }
+
+        public void Draw(World world, GameTime gameTime, Microsoft.Xna.Framework.Graphics.SpriteBatch spriteBatch)
+        {
+        }
+    }
+}
